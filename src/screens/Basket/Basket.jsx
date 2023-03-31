8b968bb845cb6f3e4a2b10d6fee6fbc5cec4af58
yash3456789 <79311343+yash3456789@users.noreply.github.com>
2023-03-31 14:02:04 +0530

    in college

---
+++ src/screens/Basket/Basket.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+function Basket() {
+  return (
+    <View>
+      <Text>Basket</Text>
+    </View>
+  );
+}
+
+export default Basket;
