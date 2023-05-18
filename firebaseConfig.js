b1c1fabd21b23afda1c51a50c334831510d5fae6
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2023-05-18 13:25:02 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,15 +6,16 @@ import { getStorage } from "firebase/storage";
 
 import { GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyACEksh0hlO9FQUAy3IjQkCwe7Cof5Il1U",
-  authDomain: "aahar-109e8.firebaseapp.com",
-  projectId: "aahar-109e8",
-  storageBucket: "aahar-109e8.appspot.com",
-  messagingSenderId: "889513020046",
-  appId: "1:889513020046:web:444d7956731ad698f597df",
-  measurementId: "G-1X3J5XKQFN",
-};
+// AAHAR API
+// const firebaseConfig = {
+//   apiKey: "AIzaSyACEksh0hlO9FQUAy3IjQkCwe7Cof5Il1U",
+//   authDomain: "aahar-109e8.firebaseapp.com",
+//   projectId: "aahar-109e8",
+//   storageBucket: "aahar-109e8.appspot.com",
+//   messagingSenderId: "889513020046",
+//   appId: "1:889513020046:web:444d7956731ad698f597df",
+//   measurementId: "G-1X3J5XKQFN",
+// };
 
 const app = initializeApp(firebaseConfig);
 
